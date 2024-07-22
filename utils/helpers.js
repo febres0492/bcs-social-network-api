8f053a2f4b998b69092f64907080c96a5faa24ab
Carlos Febres <febres0492@gmail.com>
2024-07-22 18:54:25 -0400

    Refactor helpers.js to use colors module for console.log coloring

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 const date = require('date-and-time');
+const colors = require('colors');
 
 function c(str='null', color = 'g'){ // this function is to color the console.log
-    const colors = require('colors');
     const opt = { r: 'red', g: 'green', y: 'yellow', b: 'blue'}
     return colors[opt[color]](str) 
 }
